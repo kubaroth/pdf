72816aed854062c62e2846bad480d98a4cacd872
kubaroth <kubaroth@gmail.com>
2020-09-15 17:16:05 -0700

    Added test2 which shows problems with cleanup (Dictionary) in PDFStream destructor.

--- pdftraverse.h
+++ pdftraverse.h
@@ -44,3 +44,48 @@ public:
 };
 
 
+/// NOTE: !!!!
+///  this is still required as: PDFnode<PDFArray> obj_adapt((PDFArray*)obj);  // crash and subsequent
+
+//template <>
+//class PDFnode<PDFArray> : public PDFArray {
+//public:
+//    void accept() {std::cout << "accept from PDFnodeTemplate ptr - array"<<std::endl;}
+//    PDFnode( PDFArray * inValue){
+//        auto it = inValue->GetIterator();
+//        auto i = 0;
+//        for(it.MoveNext(); !it.IsFinished(); it.MoveNext()){
+//            // std::cout  << i << " " << it.GetItem()->GetType() << std::endl;
+//            AppendObject(std::move(it.GetItem()));  //TODO: is move the right thing here?
+//            i++;
+//        }
+//        // TODO: should we cleanup input array at this point?
+//        accept();
+//    }
+//};
+
+//template <>
+//class PDFnode<PDFDictionary> : public PDFDictionary {
+//public:
+//    void accept() {std::cout << "accept from PDFnodeTemplate ptr - dictionary"<<std::endl;}
+//    PDFnode( PDFDictionary * inValue){
+//        auto it = inValue->GetIterator();
+//        for(it.MoveNext(); !it.IsFinished(); it.MoveNext()){
+//            auto key = it.GetKey();
+//            auto value = it.GetValue();
+//            Insert(key, value);
+//        }
+//        // TODO: should we cleanup input array at this point?
+//        accept();
+//    }
+//};
+
+//template <>
+//class PDFnode<PDFIndirectObjectReference> : public PDFIndirectObjectReference {
+//public:
+//    void accept() {std::cout << "accept from PDFnodeTemplate ptr - indirectRef"<<std::endl;}
+//    PDFnode(PDFIndirectObjectReference * inValue) : PDFIndirectObjectReference(inValue->mObjectID, inValue->mVersion) {
+//        accept();
+//    }
+//};
+
--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,7 +1,24 @@
 #include "catch.hpp"
 #include "../text.h"
 
+#include "../pdftraverse.h"
+
 #include <string>
+
+// Import of PDF objects in the order they appear in the enum
+#include <PDFWriter/PDFObject.h>
+#include <PDFWriter/PDFBoolean.h>
+#include <PDFWriter/PDFLiteralString.h>
+#include <PDFWriter/PDFHexString.h>
+#include <PDFWriter/PDFNull.h>
+#include <PDFWriter/PDFName.h>
+#include <PDFWriter/PDFInteger.h>
+#include <PDFWriter/PDFReal.h>
+#include <PDFWriter/PDFArray.h>
+#include <PDFWriter/PDFDictionary.h>
+#include <PDFWriter/PDFIndirectObjectReference.h>
+#include <PDFWriter/PDFStreamInput.h>
+#include <PDFWriter/PDFSymbol.h>
 
 using namespace std;
 
@@ -23,8 +40,39 @@ TEST_CASE ("test123"){
     EStatusCode StartPDFParsingStatus = parser.StartPDFParsing(pdfFile.GetInputStream());
     REQUIRE( StartPDFParsingStatus == eSuccess);
 
-    RefCountPtr<PDFDictionary> page(parser.ParsePage(page_number));
+/// without PDFnode specialization it works only if PDFDictionary is heap allocated !!!??
+TEST_CASE ("problematic heap allocation only - for dictionaries") {
+    std::string aaa("AAA");           // type 1
+    PDFLiteralString aaa_pdf(aaa);
+    PDFName aaa_name("AAA");
 
-    RefCountPtr<PDFObject> page_section(parser.QueryDictionaryObject(page.GetPtr(), "Resources"));
+    // NOTE: to test disable all the spacializations of PDFnode
+
+    // This will segafuault
+    //PDFDictionary dictionary;
+    //dictionary.Insert(&aaa_name, &aaa_pdf);
+    //auto pdf_stream = unique_ptr<PDFStreamInput> (new PDFStreamInput(&dictionary, 0));
+
+    // with unique_ptr this fails too
+    //auto dictionary = unique_ptr<PDFDictionary>(new PDFDictionary()); // needs heap allocation
+    //dictionary->Insert(&aaa_name, &aaa_pdf);
+    //auto pdf_stream = unique_ptr<PDFStreamInput> (new PDFStreamInput(dictionary.get(), 0));
+
+    // This standar pointer works but requires explicit delete once we donw with pdf_stream
+    // so there is a problem with a liftime of in the destructor of PDFStreamInput?
+    PDFDictionary * dictionary = new PDFDictionary(); // needs heap allocation
+    dictionary->Insert(&aaa_name, &aaa_pdf);
+    auto pdf_stream = unique_ptr<PDFStreamInput> (new PDFStreamInput(dictionary, 0));
+    delete dictionary;
+
+
+    // remaing of the test
+    PDFnode<PDFStreamInput> stream_node(pdf_stream.get());
+
+    auto dictionary_node = stream_node.QueryStreamDictionary();
+    auto pdfobject = dictionary_node->QueryDirectObject(aaa_pdf);
+
+    REQUIRE ( ((PDFLiteralString*)pdfobject)->GetValue().compare("AAA") == 0);
+
 
 }
